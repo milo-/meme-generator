a5122c6c1820a913027094a4d37be45751ef3ab1
futurefounder <jsk@posteo.de>
2022-09-10 10:38:06 +0200

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import NavBar from "./Navbar";
-import './App.css'
+import Meme2 from "./Meme2";
+import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-     <NavBar />
+      <NavBar />
+      <Meme2 />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import trollface from "./assets/troll-face.png";
 
 export default function NavBar() {
@@ -6,6 +5,7 @@ export default function NavBar() {
     <div className="nav--style">
       {" "}
       <img src={trollface} className="logo" alt="Vite logo" />
+      <h2 className="nav--text">Meme Generator</h2>
     </div>
   );
 }
