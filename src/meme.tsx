605e7a6dd225481ebfb8b3f2dee20b1ef5e4c864
futurefounder <jsk@posteo.de>
2022-09-10 10:50:12 +0200

    add site build

---
+++ src/meme.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import memesData from "./memesData";
+
+export default function Meme(): JSX.Element {
+  const [memeImage, setMemeImage] = React.useState();
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
+  return (
+    <main>
+      <form action="" className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          ⚡ Get a new meme image
+        </button>
+      </form>
+      <img src={memeImage} alt="" className="meme-img" />
+    </main>
+  );
+}
